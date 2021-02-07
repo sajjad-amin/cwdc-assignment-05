22fef651cb97ea65efe66441bbedf53184511165
sajjad-amin <sayem.1998.bd@gmail.com>
2021-02-07 16:06:12 +0600

    Final touch

--- script/script.js
+++ script/script.js
@@ -1,58 +1,55 @@
 const baseUrl = "https://www.themealdb.com/api/json/v1/1/";
-const inputField = document.getElementById("search-field");
+const searchField = document.getElementById("search-field");
 const searchButton = document.getElementById("search-btn");
 const displayArea = document.getElementById("display");
 const detailsArea = document.getElementById("details-area");
 
-searchButton.addEventListener("click",()=>{
-    searchFoodByName(inputField.value);
-})
-const searchFoodByName = keyword =>{
+// Handle search button click
+searchButton.addEventListener("click", ()=> searchMealsByKeyword(searchField.value));
+
+// Search meal by name or any keyword
+const searchMealsByKeyword = keyword =>{
     if (keyword != "") {
         showLoader(displayArea, true);
-        let url = `${baseUrl}search.php?s=${keyword}`;
+        const url = `${baseUrl}search.php?s=${keyword}`;
         fetch(encodeURI(url))
         .then(data=>data.json())
         .then(data=>{
             showLoader(displayArea, false);
-            displayFood(data);
+            displayMeals(data);
         });
-    }    
-}
-const displayFood = data => {
-    if (data.meals == null) {
-        showNotFoundMessage();
     } else {
-        displayArea.innerHTML = createFoodCard(data)
+        displayArea.innerHTML = "";
     }
 }
-const showNotFoundMessage = () => {
-    displayArea.innerHTML = `<h1>Not found</h1><br>
-    <span class="material-icons" style="font-size:30px;padding: 20px 10px">
-    sentiment_very_dissatisfied
-    </span>`;
-}
-const createFoodCard = data => {
-    let meals = data.meals;
+
+// Display meals according to search result
+const displayMeals = data => data.meals != null ? displayArea.innerHTML = createMealDisplayCard(data) : showNotFoundMessage();
+
+// Create html element for holding each meal item's name and thumbnail
+const createMealDisplayCard = data => {
+    const meals = data.meals;
     let elementString = "";
     meals.forEach(data => {
-            elementString += `<div class="food-item" onclick="showFoodDetails(${data.idMeal})">
-                <div class="thumbnail">
-                    <img src="${data.strMealThumb}"/>
-                </div>
-                <div class="food-name">
-                    <h3>${data.strMeal}</h3>
-                </div>
-            </div>`;
+        elementString += `<div class="food-item" onclick="showMealDetails(${data.idMeal})">
+            <div class="thumbnail">
+                <img src="${data.strMealThumb}"/>
+            </div>
+            <div class="food-name">
+                <h3>${data.strMeal}</h3>
+            </div>
+        </div>`;
     });
     return elementString;
 }
-const showFoodDetails = id => {
-    let url = `${baseUrl}lookup.php?i=${id}`;
+
+// Fetch meal details by id and show details information about the selected food
+const showMealDetails = id => {
+    const url = `${baseUrl}lookup.php?i=${id}`;
     fetch(encodeURI(url))
         .then(data=>data.json())
         .then(data=>{
-            let item = data.meals[0];
+            const item = data.meals[0];
             let ingredients = "";
             let measurement = "";
             for(let i = 1; i <= 3; i++){
@@ -63,7 +60,7 @@ const showFoodDetails = id => {
               <div class="modal-content">
                 <div class="modal-body">
                   <div class="food-details">
-                    <button id="modal-btn" onclick="hideFoodDetails()">X</button>
+                    <button id="modal-btn" onclick="hideMealDetails()">X</button>
                     <img src="${item.strMealThumb}" />
                     <div class="details">
                       <h1>${item.strMeal}</h1>
@@ -76,9 +73,13 @@ const showFoodDetails = id => {
             </section>`;
         });
 }
-const hideFoodDetails = ()=> {
-    detailsArea.innerHTML = "";
-}
-const showLoader = (parent, argument) => {
-    argument ? parent.innerHTML = `<div class="loader"></div>` : "";
-}
+
+// Show not found message if search meal not exists
+const showNotFoundMessage = () => displayArea.innerHTML = `<h1>Not found</h1><br>
+<span class="material-icons" style="font-size:30px;padding: 20px 10px">sentiment_very_dissatisfied</span>`;
+
+// Hide meal details
+const hideMealDetails = ()=> detailsArea.innerHTML = "";
+
+// Hide and show loader by passing parent element and true or false argument
+const showLoader = (parentElement, condition) => condition ? parentElement.innerHTML = `<div class="loader"></div>` : "";
